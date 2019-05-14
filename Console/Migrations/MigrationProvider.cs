8e7c98cda98c5f62b1454de650d233a474b4da5e
Stephen Fuqua <stephen.fuqua@ed-fi.org>
2019-05-14 11:28:29 -0500

    Refactor to use only a single upgrade/migration process

--- Console/Migrations/MigrationProvider.cs
+++ Console/Migrations/MigrationProvider.cs
@@ -17,57 +17,40 @@ namespace POC.DbSwitcher.Console.Migrations
 
         public void Migrate(MigrationConfig config)
         {
-            var assemblies = new List<Assembly>
+            var assemblies = new []
             {
                 Assembly.GetAssembly(typeof(POC.DbSwitcher.Database1.Marker)),
                 Assembly.GetAssembly(typeof(POC.DbSwitcher.Database2.Marker))
             };
 
-            assemblies.ForEach(x =>
+            var upgradeEngineBuilder = _migrationStrategy
+                .DeployTo(DeployChanges.To, config)
+                .WithScriptsEmbeddedInAssemblies(assemblies, filter => IsStructureFolder(filter) || IsDataFolder(filter));
+
+            config.ExtraScriptPaths
+                .ToList()
+                .ForEach(x => upgradeEngineBuilder.WithScriptsFromFileSystem(x));
+
+            var result = upgradeEngineBuilder
+                .WithExecutionTimeout(TimeSpan.FromSeconds(config.Timeout))
+                .WithTransaction()
+                .LogToConsole()
+                .Build()
+                .PerformUpgrade();
+
+            if (!result.Successful)
             {
-                DeployScripts(x, "Structure");
-                DeployScripts(x, "Data");
-            });
-
-            config.ExtraScriptPaths.ToList().ForEach(DeployExtraScripts);
-
-
-            void DeployScripts(Assembly assembly, string queryType)
-            {
-                var result = _migrationStrategy
-                    .DeployTo(DeployChanges.To, config)
-                    .WithScriptsEmbeddedInAssembly(assembly, filter => filter.Contains($"{queryType}.{_migrationStrategy.DatabaseSpecificFolderName}."))
-                    // It might be feasible to instead use WithScriptsEmbeddedInAssemblies; didn't test it out.
-                    // Doing so might be advantageous in that we would have a single transaction wrapping all
-                    // migrations, rather than one transaction per assembly.
-                    .WithExecutionTimeout(TimeSpan.FromSeconds(config.Timeout))
-                    .WithTransaction()
-                    .LogToConsole()
-                    .Build()
-                    .PerformUpgrade();
-
-                if (!result.Successful)
-                {
-                    throw result.Error;
-                }
+                throw result.Error;
             }
 
-            void DeployExtraScripts(string path)
-            {
-                // As with comment above, probably better to combine into a single call
-                // so that all migrations are in a single transaction.
-                var result = _migrationStrategy
-                    .DeployTo(DeployChanges.To, config)
-                    .WithScriptsFromFileSystem(path)
-                    .WithTransaction()
-                    .LogToConsole()
-                    .Build()
-                    .PerformUpgrade();
 
-                if (!result.Successful)
-                {
-                    throw result.Error;
-                }
+            bool IsStructureFolder(string filter)
+            {
+                return filter.Contains($"Structure.{_migrationStrategy.DatabaseSpecificFolderName}.");
+            }
+            bool IsDataFolder(string filter)
+            {
+                return filter.Contains($"Data.{_migrationStrategy.DatabaseSpecificFolderName}.");
             }
         }
     }
